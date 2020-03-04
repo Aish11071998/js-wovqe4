5d22f9647e975764572130a1806c48369aeb55fa
Aish11071998 <aishwarya.sekar@ducenit.com>
2020-03-04 17:26:57 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var x=prompt('Enter the name');
+var a=prompt('enter start index');
+if(isNaN(a))
+{
+  alert('Enter only numbers')
+  var a=prompt('enter start index');
+
+}
+
+var b=prompt('enter end index');
+  if(isNaN(b))
+  {
+  alert('Enter only numbers')
+  var b=prompt('enter end index');
+  
+  }
+   var y=x.slice(a,b);
+alert(y);
+
